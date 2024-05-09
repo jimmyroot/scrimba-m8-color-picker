702b1bf6cedf8d4bcb6ec0453e64f1c739aa4a63
Jimmy Newman <jimmy@Jimmys-MacBook-Pro.local>
2024-05-10 00:02:33 +0100

    Fixed nav media query bug

--- layout/header.js
+++ layout/header.js
@@ -41,19 +41,17 @@ const Header = () => {
                     <img class="header-logo-img" src="/logo.png" data-type="refresh" alt="Color Cult logo, a circle containing a color swatch">
                     <h1 class="header-logo" data-type="refresh">Color Cult</h1>
                 </div>
-                <div>
-                    <ul class="header-menu" id="menu">
-                        <li>
-                            <a href="#">Tools</a>
-                        </li>
-                        <li>
-                            <a href="#">Sign in</a>
-                        </li>
-                        <li>
-                            <a class="sign-up" href="#">Sign up</a>
-                        </li>
-                    </ul>
-                </div>
+                <ul class="header-menu" id="menu">
+                    <li>
+                        <a href="#">Tools</a>
+                    </li>
+                    <li>
+                        <a href="#">Sign in</a>
+                    </li>
+                    <li>
+                        <a class="sign-up" href="#">Sign up</a>
+                    </li>
+                </ul>
                 <button class="hamburger hamburger--3dy" id="hamburger" type="button" data-type="hamburger">
                     <span class="hamburger-box">
                         <span class="hamburger-inner"></span>
